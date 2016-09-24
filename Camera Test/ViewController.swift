fd6c8c47299e2f92ac6bada5a24ca1186fabc86c
Kunal Thacker <kunal.thacker@gdn-commerce.com>
2016-09-24 13:23:49 +0530

    Changed bundle identifier, added nil check in getPixelColor

--- Camera Test/ViewController.swift
+++ Camera Test/ViewController.swift
@@ -12,18 +12,21 @@ import CoreGraphics
 extension UIImage {
     func getPixelColor(pos: CGPoint) -> UIColor {
 
-        let pixelData = self.cgImage!.dataProvider!.data
-        let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+        if let pixelData = self.cgImage?.dataProvider?.data {
+            let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
 
-        let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+            let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
 
-        let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
-        let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
-        let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
-        let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+            let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+            let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+            let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+            let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
 
-        return UIColor(red: r, green: g, blue: b, alpha: a)
-    }  
+            return UIColor(red: r, green: g, blue: b, alpha: a)
+        }
+
+        return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
 }
 
 extension UIColor {
